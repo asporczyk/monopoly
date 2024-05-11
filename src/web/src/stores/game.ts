e6d90a35e49eb5732b6c89351fac50a85efc3057
Adam Sporczyk <adamspo00@gmail.com>
2024-05-11 20:32:53 +0200

    #21 Add players UI and add pawns with their posiotions on board

--- src/web/src/stores/game.ts
+++ src/web/src/stores/game.ts
@@ -2,6 +2,13 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useGameStore = defineStore('game', () => {
+  const players = ref<Player[]>([
+    { id: 1, name: 'Player 1', bank: 100, color: 'red', position: 0 },
+    { id: 2, name: 'Player 2', bank: 200, color: 'blue', position: 1 },
+    { id: 3, name: 'Player 3', bank: 150, color: 'green', position: 2 },
+    { id: 4, name: 'Player 4', bank: 1000, color: 'yellow', position: 3 },
+  ])
+
   const lastDiceRoll = ref(0)
 
   const isLastRollDouble = ref(false)
@@ -11,5 +18,5 @@ export const useGameStore = defineStore('game', () => {
     isLastRollDouble.value = rolls[0] === rolls[1]
   }
 
-  return { lastDiceRoll, isLastRollDouble, saveDiceRoll }
+  return { lastDiceRoll, isLastRollDouble, saveDiceRoll, players }
 })
