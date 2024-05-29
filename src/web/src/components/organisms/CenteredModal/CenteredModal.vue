e77bd599ac0e3ad2e8a312d68f68d9ffa6efdf7f
Adam Sporczyk <adamspo00@gmail.com>
2024-05-29 17:31:02 +0200

    #30 Add API integration for moving players on board

--- src/web/src/components/organisms/CenteredModal/CenteredModal.vue
+++ src/web/src/components/organisms/CenteredModal/CenteredModal.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import BasicModal from '@/components/atoms/BasicModal/BasicModal.vue'
 import CenteredBox from '@/components/molecules/CenteredBox/CenteredBox.vue'
-import IconButton from '@/components/atoms/Buttons/IconButton.vue'
 import HeadlineM from '@/components/atoms/Typography/HeadlineM.vue'
 import TextBody from '@/components/atoms/Typography/TextBody.vue'
 
@@ -15,9 +14,6 @@ defineEmits(['on-close'])
       :height="height ?? 'auto'"
       :max-width="maxWidth"
     >
-      <div class="position-absolute align-self-end">
-        <IconButton icon="mdi-close" @click="$emit('on-close')" />
-      </div>
       <div class="d-flex flex-column align-center fill-height px-lg-5">
         <HeadlineM class="mx-2 my-2 text-center">
           <slot name="title" />
